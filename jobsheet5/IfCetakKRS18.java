55888cb309853db54a7af5b002547f1e70c2d54a
“rendiizaqi” <rendirealme34@gmail.com>
2025-10-02 10:19:47 +0700

    Percobaan 1

--- jobsheet5/IfCetakKRS18.java
+++ jobsheet5/IfCetakKRS18.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IfCetakKRS18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("--- Cetak KRS Siakad ---");
+        System.out.print("---- Cetak KRS Siakad ----");
         System.out.print("Apakah UKT anda sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
