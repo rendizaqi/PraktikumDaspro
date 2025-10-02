c60692953980c8e0fba84cd75a4b1f603d98ca82
“rendiizaqi” <rendirealme34@gmail.com>
2025-10-02 18:31:07 +0700

    Tugas 1

--- jobsheet5/IfCetakKRS18.java
+++ jobsheet5/IfCetakKRS18.java
@@ -3,19 +3,10 @@ import java.util.Scanner;
 public class IfCetakKRS18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("---- Cetak KRS Siakad ----");
+        System.out.println("---- Cetak KRS Siakad ----");
         System.out.print("Apakah UKT anda sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran terverifikasi");
-            System.out.println("Silakan cetal KRS dan minta tanda tangan DPA");
-
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-
-        sc.close();
+        System.out.println(uktLunas ? "Pembayaran terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak, Silakan lunasi UKT terlebih dahulu");
+        }
     }
-}
-
-}
